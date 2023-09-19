beb10bdbc53964df0ade73247c208a9accc2082f
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2023-09-19 13:51:11 -0700

    Create grid using slider value
    Gets the input from slider and create grid out of the given value. When
    slider value is updated the makeGrid() function will create a new grid and
    append it to the existing grid for now, still working on that issue.

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-
-
-
+//show slider value
 let slider = document.getElementById("pixel");
 let output = document.getElementById("gridVal");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerText = slider.value + "x" + slider.value; // Display the default slider value
+
+//create grid
+const container = document.getElementById("container");
+
+function makeGrids(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (g = 0; g < (rows*cols); g ++) {
+        let cell = document.createElement("div");
+        cell.innerText = (g + 1);
+        container.appendChild(cell).className = "grid-item";
+    }
+}
+
+makeGrids(slider.value, slider.value);
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerText = this.value + "x" + this.value;
-}
+    output.innerText = this.value + "x" + this.value;
+    //let element = document.getElementsByClassName("grid-item");
+    //if (element) {container.parentNode.removeChild(container);};
+    makeGrids(this.value, this.value);
+  }
