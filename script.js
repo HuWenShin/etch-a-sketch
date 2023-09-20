8c5b50e55fb5568f7f5ba9548ee584d8351373e8
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2023-09-19 18:25:40 -0700

    Add color function to paint
    Added color function so when mouse is down and over the canvas it changes
    the color of grids to paint. Color picker function and erase button still
    under development.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //show slider value
-let slider = document.getElementById("pixel");
-let output = document.getElementById("gridVal");
+var slider = document.getElementById("pixel");
+var output = document.getElementById("gridVal");
 output.innerText = slider.value + "x" + slider.value; // Display the default slider value
 
 //create grid
@@ -9,9 +9,10 @@ const container = document.getElementById("container");
 function makeGrids(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
-    for (g = 0; g < (rows*cols); g ++) {
+    for (i = 0; i < (rows*cols); i ++) {
         let cell = document.createElement("div");
-        cell.innerText = (g + 1);
+        cell.addEventListener("mouseover", color);
+        cell.addEventListener("mousedown", color);
         container.appendChild(cell).className = "grid-item";
     }
 }
@@ -22,7 +23,20 @@ makeGrids(slider.value, slider.value);
 slider.oninput = function() {
     output.innerText = this.value + "x" + this.value;
     if (container.hasChildNodes()) {
-        while (container.firstChild) {container.removeChild(container.firstChild);}
+        while (container.firstChild) {container.removeChild(container.firstChild);};
     };
     makeGrids(this.value, this.value);
   }
+
+//color
+var mouseDown = false;
+document.body.onmousedown = function() {mouseDown = true;};
+document.body.onmouseup = function() {mouseDown = false;};
+
+function color(event) {
+    if (mouseDown) {
+        event.target.style.backgroundColor = "green";
+    }
+}
+
+//erase button
